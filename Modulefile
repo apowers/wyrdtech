57ec0aad76c20b9255325ffb9f60fd213faae747
Atom Powers <atom.powers@gmail.com>
2013-04-15 15:17:07 -0700

    documentation cleanup

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name 'apowers-basichost'
-version '2013.04.15'
+version '0.1.1'
 summary 'Basic host configuration'
 description 'Basic host configuration: interfaces, dhclient, /etc/skel, resolv.conf. Fully paramaterized and hiera integrated.'
 project_page 'https://github.com/apowers/basichost/'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,16 @@
 class basichost (
-    $default_gateway = ${::default_gateway},
-    $iface_dir = $basichost::params::iface_dir,
-    $iface_file = $basichost::params::iface_file,
-    $dhclient_directory  = $basichost::params::dhclient_directory,
-    $dhclient_file  = $basichost::params::dhclient_file,
+    $iface_dir          = $basichost::params::iface_dir,
+    $iface_file         = $basichost::params::iface_file,
+    $dhclient_directory = $basichost::params::dhclient_directory,
+    $dhclient_file      = $basichost::params::dhclient_file,
     $dhclient_hasstatus = $basichost::params::dhclient_hasstatus,
-    $dhclient_start = $basichost::params::dhclient_start,
-    $dhclient_stop = $basichost::params::dhclient_stop,
-    $packages = ['bash', 'tmux'],
-    $resolv_conf = {'domain'=>'','search'=>'','nameservers'=>[]},
-    $directories = ['/home'],
-    $static_address = {},
+    $dhclient_start     = $basichost::params::dhclient_start,
+    $dhclient_stop      = $basichost::params::dhclient_stop,
+    $packages           = ['bash', 'tmux'],
+    $resolv_conf        = {'domain'=>'','search'=>'','nameservers'=>[]},
+    $directories        = ['/home'],
+    $static_address     = {},
 ) inherits basichost::params {
-
-    # Default file mode
-    File { mode => '644', }
 
     # Base System Packages
     package { $packages: ensure=>present }
@@ -22,14 +18,17 @@ class basichost (
     # Network Interfaces
     file { $iface_dir :
         ensure  => 'directory',
+        mode => '0644',
     }
 
     file { "${iface_dir}/${iface_file}" :
         content => template("basichost/${osfamily}/${iface_file}"),
+        mode => '0644',
     }
 
     file { "${dhclient_directory}/${dhclient_file}" :
         content => template("basichost/dhclient.conf"),
+        mode => '0644',
     }
 
     service { 'dhclient':
@@ -42,24 +41,29 @@ class basichost (
     # Name Resolution
     file { "/etc/resolv.conf" :
         content => template("basichost/resolv.conf"),
+        mode => '0644',
     }
 
     # System Directories
     file { $directories :
         ensure  => 'directory',
+        mode => '0644',
     }
 
     # User Environment
     file { '/etc/skel/.inputrc' :
         source => 'puppet:///modules/basichost/inputrc',
+        mode => '0644',
     }
 
     file { '/etc/skel/.bashrc' :
         source => 'puppet:///modules/basichost/bashrc',
+        mode => '0644',
     }
 
     file { '/etc/skel/.profile' :
         source => 'puppet:///modules/basichost/profile',
+        mode => '0644',
     }
 
 }
